182ae5a1c72bd076992827b5f10a2ccde115268e
Eganin <egorzaharin12@gmail.com>
2021-01-01 23:13:21 +0500

    add api

--- moviesapp/src/main/java/com/example/moviesapp/data/models/Genre.kt
+++
@@ -1,3 +0,0 @@
-package com.example.moviesapp.data.models
-
-data class Genre(val id: Int, val name: String)
---
+++ moviesapp/src/main/java/com/example/moviesapp/network/api/MovieApi.kt
@@ -0,0 +1,32 @@
+package com.example.moviesapp.network.api
+
+import com.example.moviesapp.network.RetrofitModule.API_KEY
+import com.example.moviesapp.network.RetrofitModule.DEFAULT_LANGUAGE
+import com.example.moviesapp.network.RetrofitModule.PARAM_API_KEY
+import com.example.moviesapp.network.RetrofitModule.PARAM_LANGUAGE
+import com.example.moviesapp.network.RetrofitModule.PARAM_PAGE
+import com.example.moviesapp.pojo.configuration.ResponseConfiguration
+import com.example.moviesapp.pojo.movies.details.ResponseMovieDetail
+import com.example.moviesapp.pojo.movies.popular.ResponseMovies
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieApi {
+    @GET("/configuration")
+    suspend fun getConfiguration(@Query(PARAM_API_KEY) apiKey: String = API_KEY): List<ResponseConfiguration>
+
+    @GET("/movie/popular")
+    suspend fun getPopularMovies(
+        @Query(PARAM_API_KEY) apiKey: String = API_KEY,
+        @Query(PARAM_LANGUAGE) language: String = DEFAULT_LANGUAGE,
+        @Query(PARAM_PAGE) page: String
+    ): List<ResponseMovies>
+
+    @GET("/movie/{movie_id}")
+    suspend fun getDetailInfo(
+        @Query(PARAM_API_KEY) apiKey: String = API_KEY,
+        @Query(PARAM_LANGUAGE) language: String = DEFAULT_LANGUAGE,
+        @Path("movie_id") movieId: String
+    ): List<ResponseMovieDetail>
+}
